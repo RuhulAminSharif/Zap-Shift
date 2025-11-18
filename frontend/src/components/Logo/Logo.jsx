0dbc3e1777fe237a05ca36bc2bd20c6b8bad9b95
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2025-11-18 20:55:44 +0600

    feat: assets added and router set up

---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from "../../assets/logo.png"
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt="Logo" srcset="" />
+    </div>
+  );
+};
+
+export default Logo;
--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello</div>,
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
   },
 ]);
