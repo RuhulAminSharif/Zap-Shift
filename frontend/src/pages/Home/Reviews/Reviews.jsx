0a7905a260792fca5b7ec1794bc4e41ef9b4368d
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2025-11-19 20:21:28 +0600

    feat: reviews section added

---
+++ frontend/src/pages/Home/Reviews/Reviews.jsx
@@ -0,0 +1,51 @@
+import React, { use } from "react";
+import { Autoplay, EffectCoverflow, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ReviewCard from "./ReviewCard";
+
+const Reviews = ({ reviewsPromise }) => {
+  const reviews = use(reviewsPromise);
+  return (
+    <div className="my-24">
+      <div className="text-center mb-24">
+        <h3 className="text-3xl text-center font-bold my-8">Reviews</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          molestias totam optio tempore ipsum consequatur hic ex rerum quisquam
+          quam, nisi aliquam laboriosam. Quo nobis molestias cumque sit,
+          laboriosam tempore?
+        </p>
+      </div>
+      <Swiper
+        loop={true}
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 30,
+          stretch: "50%",
+          depth: 200,
+          modifier: 1,
+          scale: 0.75,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        {reviews.map((review) => (
+          <SwiperSlide key={review.id}>
+            <ReviewCard review={review}></ReviewCard>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Reviews;
