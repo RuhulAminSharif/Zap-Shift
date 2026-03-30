2f95839627c77d28fc036ff1c3f446e7662bc536
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2026-03-30 13:02:14 +0600

    feat: implement rider approval functionality with API integration and UI updates

--- backend/index.js
+++ backend/index.js
@@ -230,6 +230,16 @@ async function run() {
     });
 
     // riders related apis
+    app.get("/riders", async (req, res) => {
+      const query = {};
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+      const cursor = ridersCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/riders", async(req, res) => {
       const rider = req.body
       rider.status = "pending"
@@ -238,6 +248,35 @@ async function run() {
       const result = await ridersCollection.insertOne(rider)
       res.send(result)
     })
+
+    app.patch("/riders/:id", verifyFBToken, async (req, res) => {
+      const status = req.body.status;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+
+      const result = await ridersCollection.updateOne(query, updatedDoc);
+
+      if (status === "approved") {
+        const email = req.body.email;
+        const userQuery = { email };
+        const updateUser = {
+          $set: {
+            role: "rider",
+          },
+        };
+        const userResult = await usersCollection.updateOne(
+          userQuery,
+          updateUser,
+        );
+      }
+
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
